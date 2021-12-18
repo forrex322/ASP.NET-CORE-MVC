203b7528337b58edceb96f35ca3e5381b38edc4c
Dima Lol <zonex1995@gmail.com>
2021-12-18 12:57:01 +0200

    added topbar

---
+++ Dima/Views/Shared/Components/Topbar/TopbarViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dima.Views.Shared.Components.Topbar
+{
+    public class TopbarViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            TopbarViewModel vm = new TopbarViewModel()
+            {
+                IsAdminUser = false
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ Dima/Views/Shared/Components/Topbar/TopbarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dima.Views.Shared.Components.Topbar
+{
+    public class TopbarViewModel
+    {
+        public bool IsAdminUser { get; set; }
+    }
+}
